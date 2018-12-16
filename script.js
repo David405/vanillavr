5f284ea636d23f0369f07160c19e2e2823e28627
david405 <davidasamonye@gmail.com>
2018-12-16 11:18:14 +0100

    finalized the styling, added word content and animation

--- script.js
+++ script.js
@@ -6,3 +6,16 @@ function openNav() {
     document.getElementById("myNav").style.width = "0%";
   }
   
+$(document).ready(function(){ 
+  $(window).scroll(function(){ 
+      if ($(this).scrollTop() > 100) { 
+          $('#scroll').fadeIn(); 
+      } else { 
+          $('#scroll').fadeOut(); 
+      } 
+  }); 
+  $('#scroll').click(function(){ 
+      $("html, body").animate({ scrollTop: 0 }, 600); 
+      return false; 
+  }); 
+});
